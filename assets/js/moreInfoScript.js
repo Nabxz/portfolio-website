0e97be8ea608ae1feed8d4ebdb9d7af6b8ad57ff
Nabxz <mammanidev@gmail.com>
2025-07-02 23:42:46 -0500

    Fixed bug with back button and updated resume link on more info pages

--- assets/js/moreInfoScript.js
+++ assets/js/moreInfoScript.js
@@ -1,12 +1,16 @@
 // BACK BUTTON
 // Event listener for the back button
-document.getElementById('backButton').addEventListener('click', function() {
+document.getElementById('backButton').addEventListener('click', function () {
+  if (window.history.length > 1) {
     window.history.back();
+  } else {
+    window.location.href = '../index.html#portfolio'; // fallback route
+  }
 });
 
 // Event listener for the "Back to Top" button
-document.getElementById('backToTopButton').addEventListener('click', function() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+document.getElementById('backToTopButton').addEventListener('click', function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 // ===============================/ BACK BUTTON /===
 
@@ -20,8 +24,8 @@ var images = document.querySelectorAll('.project-images-container img');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
-images.forEach(function(img) {
-  img.onclick = function(){
+images.forEach(function (img) {
+  img.onclick = function () {
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
@@ -32,12 +36,12 @@ images.forEach(function(img) {
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
